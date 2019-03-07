21c07a309333320d6b9fc64fd7263a2287b86a4f
Connie Chan <conniechan.10@gmail.com>
2019-03-07 00:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ myMain.remove()
 
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let newHeader = document.createElement('h1')
+// <h1><h1>
+newHeader.id = "victory"
+// <h1 style="background-color: red">
+// <h1 id="victory"></h1>
+newHeader.innerHTML = "Not sure what is going on"
