43dc66839a21fbc00a2b58d5390207f783ab455a
Connie Chan <conniechan.10@gmail.com>
2019-03-07 00:20:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
+let myMain = document.querySelector('#main')
+myMain.remove()
+
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let newHeader = document.createElement('h1')
+// <h1><h1>
+newHeader.id = "victory"
+// <h1 style="background-color: red">
+// <h1 id="victory"></h1>
+newHeader.innerText = "Connie is the champion"
+document.body.appendChild(newHeader)
