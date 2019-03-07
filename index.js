700edf9b90df8129f77941c0e4ec88be7174a047
Connie Chan <conniechan.10@gmail.com>
2019-03-07 00:04:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
+let myMain = document.querySelector('#main')
+
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
